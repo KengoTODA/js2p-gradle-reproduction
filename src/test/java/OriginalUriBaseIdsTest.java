9e908f65858d6ad79be16fdcbd786c64388b4fa8
Kengo TODA <skypencil@gmail.com>
2020-09-04 09:12:25 +0800

    first commit

---
+++ src/test/java/OriginalUriBaseIdsTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.OriginalUriBaseIds;
+
+import org.junit.jupiter.api.Test;
+
+class OriginalUriBaseIdsTest {
+	@Test
+	void checkAdditionalProperties() {
+        OriginalUriBaseIds baseIds = new OriginalUriBaseIds();
+	}
+}
